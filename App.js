97b52662c9e18245a73166d74fb8f60f29b339a5
Rutger <rutger.cobben@ca.com>
2017-11-02 14:25:40 +0000

    bla

---
+++ App.js
@@ -0,0 +1,10 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items: {html: "Rally Test HTML"},
+    launch: function() {
+        //Write app code here
+    console.log("First entry log here2");
+        //API Docs: https://help.rallydev.com/apps/2.1/doc/
+    }
+});
